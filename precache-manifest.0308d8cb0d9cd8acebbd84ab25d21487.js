92754f199ed0afe91a0182e83dc96da8f408597b
Glen <mistergjones@yahoo.com.au>
2020-10-18 20:24:39 +1100

    Updates

---
+++ precache-manifest.0308d8cb0d9cd8acebbd84ab25d21487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534b85502bc9f14edb449fabf0c1c390",
+    "url": "/codepen-app/index.html"
+  },
+  {
+    "revision": "612dceaa932db0352886",
+    "url": "/codepen-app/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "263e432c752600b1c277",
+    "url": "/codepen-app/static/css/main.6ac89577.chunk.css"
+  },
+  {
+    "revision": "612dceaa932db0352886",
+    "url": "/codepen-app/static/js/2.27c9133a.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/codepen-app/static/js/2.27c9133a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263e432c752600b1c277",
+    "url": "/codepen-app/static/js/main.acae7f87.chunk.js"
+  },
+  {
+    "revision": "669a128cc5826abd066f",
+    "url": "/codepen-app/static/js/runtime-main.c2fa444a.js"
+  }
+]);
